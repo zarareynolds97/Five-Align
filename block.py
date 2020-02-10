76557418a77212552d8d8e61b188564db0e30654
zarareynolds97 <59961359+zarareynolds97@users.noreply.github.com>
2020-02-10 15:40:40 +0000

    workingcode

--- block.py
+++ block.py
@@ -1,10 +1,7 @@
-import pygame
-import random
-import time
-import math
+import pygame, random, time, math 
 
-screen_width = 600
-screen_height = 600
+screen_width = 400
+screen_height = 400
 screen = pygame.display.set_mode([screen_width, screen_height]) 
 
 BLACK = (0,0,0)
@@ -22,61 +19,58 @@ def random_color():
 
 screen.fill(WHITE)
 
-blocks = {} 
+
 
 def starterBlocks():
     for i in range(6): 
-        A = random.randint(0,600)
+        A = random.randint(0,screen_width)
         finalA = int(round(A/25.0)) * 25
-        B = random.randint(0,600)
+        B = random.randint(0,screen_height)
         finalB = int(round(B/25.0)) * 25
-        blocks.append(pygame.draw.rect(screen, random_color(), (finalA, finalB, 25,25), 0))
+        global rectangle
+        rectangle = pygame.draw.rect(screen, random_color(), (finalA, finalB, 25,25), 0)
         pygame.display.update()
 
 #i want to be able to call another 3 random pieces up -- need to be a function to call
 def newRandom():
     #BUT not in the points where there are already stuff - HOW TO DETECT THESE THINGS 
     for i in range(3):
-        A = random.randint(0,600)
+        A = random.randint(0,screen_width)
         finalA = int(round(A/25.0)) * 25
-        B = random.randint(0,600)
+        B = random.randint(0,screen_height)
         finalB = int(round(B/25.0)) * 25
         newRect = pygame.draw.rect(screen, random_color(), (finalA, finalB, 25, 25), 0)
-    
 
 
-def determine_mouseOver(valx, valy):
-    if blocks.collidepoint(valx, valy): #myRectangle ERROR name not defined 
-        return True
-    else:
-        return False
+
+
 
      
 
 def main():
-    starterBlocks() 
+    starterBlocks() #calls the function which places five blocks on screen at start of programme 
     
     running = True 
-    while running:
+    while running: 
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
                 sys.exit()
+
+                
                 
             if event.type == pygame.MOUSEBUTTONDOWN: 
                 finalX, finalY = pygame.mouse.get_pos()
-            
                 
-                mouseOver = determine_mouseOver(finalX, finalY)
-                if mouseOver == True:
+                if rectangle.collidepoint(finalX, finalY):
                     a = int(round(finalX/25.0)) * 25
                     b = int(round(finalY/25.0)) * 25
                     pygame.draw.rect(screen, random_color(), (a, b, 25, 25), 0)
                     newRandom() 
                     
-                elif mouseOver == False:
+                else:
                     a = int(round(finalX/25.0)) * 25
                     b = int(round(finalY/25.0)) * 25
                     pygame.draw.rect(screen, BLACK, (a, b, 25,25), 0) 
@@ -90,7 +84,7 @@ def main():
                 
 
                 
-    #why does it only affect for one rectangle? I may need to change to a loop 
+
       
 
 
